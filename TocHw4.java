2669e019317048ddd8ae6a78f782edcc6aee6d20
art159201 <art159201@users.noreply.github.com>
2014-06-26 20:57:24 +0800

    Update TocHw4.java

--- TocHw4.java
+++ TocHw4.java
@@ -9,6 +9,8 @@ import java.util.regex.*;
 public class TocHw4
 
 {
+	
+	
 	public static String road(String t)
 	{
 		if(t.contains("路"))
@@ -33,132 +35,169 @@ public class TocHw4
 		}
 		else
 		{
-			return "";
+			return null;
 		}
 		
 		return t;
 	}
 	
 
-	public static void  main(String[] args) throws IOException, JSONException
+	public static void  main(String[] args, int o) throws IOException, JSONException
 	{
 		
 		String url=new String(args[0]);
+		String sect;
+		String road;
+		int year,price;
+		
 		URL pageUrl = new URL(url);
 		URLConnection myconnection = pageUrl.openConnection();
 		myconnection.connect();
+		int max_distinct_month=0,i=0,count=0,temp=0;
 
 		BufferedReader data = new BufferedReader(new InputStreamReader(pageUrl.openStream(),"UTF-8"));
-		
-		
-		
 		JSONTokener tok = new JSONTokener(data);
 		JSONArray arr=new JSONArray(tok);
-		String mdroad;
-		int max_distinct_month=0,i=0,count=0,temp=0;
-		int [] check;
-		check= new int[arr.length()];
-		for(i=0;i<arr.length();i++)
-		{
-			check[i]=1;
-		}
-		
-		Pattern pattern;
-		int [] montemp;
-		montemp=new int [2000];
-		
+		String[] temps;
+		temps=new String[20000];
+		int[] tempm;
+		tempm=new int[20000];
+		String[] temps2;
+		temps2=new String[20000];
+		int[][] temp1;
+		temp1=new int[20000][20000];
+		int[] ans;
+		ans=new int [20000];
+		String[] anss;
+		anss=new String[20000];
 		
 		while(i<arr.length())
 		{
 			JSONObject obj =arr.getJSONObject(i);
+			sect=obj.getString("鄉鎮市區");
+			road=obj.getString("土地區段位置或建物區門牌");
+			//year=
+			price=obj.getInt("總價元");
 			
-			//if(check[i]!=0)
+			if(road.indexOf("路")>0)
+			{
+				temps[count]=road(sect);
+				tempm[count]=obj.getInt("交易年月");
+				count++;
+			}
+			else if(road.indexOf("街")>0)
+			{
+				temps[count]=road(sect);
+				tempm[count]=obj.getInt("交易年月");
+				count++;
+			}
+			else if(road.indexOf("大道")>0)
+			{
+				temps[count]=road(sect);
+				tempm[count]=obj.getInt("交易年月");
+				count++;
+			}
+			else if(road.indexOf("巷")>0)
+			{
+				temps[count]=road(sect);
+				tempm[count]=obj.getInt("交易年月");
+				count++;
+			}
+			int k,l = 0,m = 0,n=0;
+			for(int j=0;j<count;j++)
+			{
+				for(k=0;k<j;k++)
 				{
-					pattern=Pattern.compile(road(obj.optString("土地區段位置或建物區門牌")));
-					Matcher match1 =pattern.matcher(obj.optString("土地區段位置或建物區門牌"));
-					if(match1.find())
+					if(temps[j].equals(temps[k]))
 					{
-						montemp[count]=obj.getInt("交易年月");
-						count++;
-						
-					}
-					for(int j=1;j<count;j++)
-					{
-						for(int k=0;k<j;k++)
-						{
-							if(montemp[j]==montemp[k])
+						if(temps2.length!=0)
+							for(l=0;l<temps2.length;l++)
 							{
-								montemp[j]=0;								
+								if(temps2[l].equals(temps[j]))
+									if(temp1[l].length!=0)
+										{
+											for(m=0;m<temps2.length;l++)
+											{
+												if(temp1[l][m]==tempm[j])
+													break;
+											}	
+											break;
+										}
 							}
+						else if(temp1[l][m]==0)
+						{
+							temp1[l][m]=tempm[j];
+							ans[l]++;
 							
 						}
 						
+						
+						
 					}
 					
-					for(int j=0;j<count;j++)
-					{
-						if(montemp[j]!=0)
-							temp++;
-					}
 					
-					if(temp>max_distinct_month)
-						{
-							max_distinct_month=temp;
-							mdroad=road(obj.optString("土地區段位置或建物區門牌");
-						}
 				}
-			i++;
-		}
 		
-		
-		/*
-		//System.out.println(max_distinct_month);
-		Pattern pattern=Pattern.compile(Integer.toString(max_distinct_month));
-		i=0;
-		int [] price;
-		price=new int [2000];
-		String[] temp;
-		temp=new String[2000];
-		
-		while(i<arr.length())
-		{
-			JSONObject obj =arr.getJSONObject(i);
+				if(j==k)
+				{
+					anss[n]=temps[j];
+					temp1[n][0]=tempm[j];
+					ans[n]=1;
+					n++;
 			
-			if(obj.getInt("交易年月")==max_distinct_month)
+				}
+		
+			}	
+			
+			int[] number=new int[3];
+			
+			for(k=0;anss[k]!=null;k++)
 			{
-				temp[count]=obj.getString("土地區段位置或建物區門牌");
-				price[count]=obj.getInt("總價元");
-				System.out.println(temp[count]);
-				System.out.println(obj.getInt("交易年月"));
-				System.out.println(price[count]);
-				
-				count++;
+				if(l<ans[k])
+				{
+					l=ans[k];
+					number[0]=k;
+				}
+
+			}
+			int q=0;m=0;
+			for(q=number[0]+1;anss[q]!=null;q++)
+			{
+				if(ans[q]==l)
+				{
+					m++;
+					number[m]=q;
+				}
+			}
+
+			
+		int highPrice=0,lowPrice=0;
+		String  ansroad;
+		for(m=0;number[m]!=0;m++)
+		{
+			ansroad=anss[number[m]];
+			lowPrice=obj.getInt("總價元");
+			for(o=0;o<arr.length();o++)
+			{
+				obj=arr.getJSONObject(o);
+				sect=obj.getString("土地區段位置或建物區門牌");
+				price=obj.getInt("總價元");
+				if(sect.contains(ansroad))
+				{	
+					for(q=0;q<50;q++)
+					{
+						if(price<lowPrice)
+							lowPrice=price;
+						if(price>highPrice)
+							highPrice=price;
+					}
+		
+				}
+		
 			}
 			
-			i++;
+			System.out.println(ansroad+", 最高成交價: "+highPrice+", 最低成交價: "+lowPrice);
 		}
-		
-		
-		
-				
-		i=0;
-		int lowPrice=price[0],highPrice=price[0];
-		while(i<count)
-		{
-			if(lowPrice>price[i])
-				lowPrice=price[i];
-			if(highPrice<price[i])
-				highPrice=price[i];
-			
-			i++;
-		}*/
-		/*i=0;
-		while(i<count){
-		System.out.print(temp[count]+":");
-		System.out.println(price[count]);}*/
 	}
-	
-	
-	
-	
+}
 }
